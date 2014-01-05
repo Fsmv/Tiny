e2da94d0d8207f23ddf2570b696cfc74bd732027
Andrew Kallmeyer <kallmeyeras@gmail.com>
2014-01-04 21:32:35 -0500

    Made the example Main work

--- Tiny2D/Java/src/net/sapium/tiny/Main.java
+++ Tiny2D/Java/src/net/sapium/tiny/Main.java
@@ -2,34 +2,20 @@ package net.sapium.tiny;
 
 import java.applet.Applet;
 
-import net.sapium.tiny.utils.InputHandler;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
 public class Main extends Applet {
     private static final long serialVersionUID = 1L;
 
-    public static final String logfile = "output.log";
-
-    private static Logger logger;
     private static Game game;
 
     public static void main(String[] args) {
         game = new Game();
         game.createWindow();
+        game.start();
     }
 
     @Override
     public void init() {
-        logger.setLevel(Level.ALL);
-
         game = new Game();
-        
-        InputHandler ih = new InputHandler();
-        game.addKeyListener(ih);
-        game.addMouseListener(ih);
-        game.addMouseMotionListener(ih);
         
         this.add(game);
         game.init();

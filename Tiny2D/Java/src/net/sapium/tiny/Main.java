8c3f01440fffc9e4cfacb7c5e3ec90bc94b3fb1b
Andrew Kallmeyer <kallmeyeras@gmail.com>
2014-01-04 21:23:46 -0500

    Made the example Main work

--- Tiny2D/Java/src/net/sapium/tiny/Main.java
+++ Tiny2D/Java/src/net/sapium/tiny/Main.java
@@ -1,22 +1,13 @@
 package net.sapium.tiny;
 
 import java.applet.Applet;
-import java.awt.Frame;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import net.sapium.tiny.utils.InputHandler;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
 
-public class Main extends Applet implements WindowListener {
+public class Main extends Applet {
     private static final long serialVersionUID = 1L;
 
     public static final String logfile = "output.log";
@@ -25,52 +16,12 @@ public class Main extends Applet implements WindowListener {
     private static Game game;
 
     public static void main(String[] args) {
-        Frame window = new Frame(Game.NAME);
-
-        initLog4J();
-        logger.setLevel(Level.INFO);
-        if (args.length > 0) {
-            if (args[0] == "--debug") {
-                logger.setLevel(Level.ALL);
-            }
-        }
-
-        
-        window.addWindowListener(new Main());
-        
         game = new Game();
-        
-        InputHandler ih = new InputHandler();
-        game.addKeyListener(ih);
-        game.addMouseListener(ih);
-        game.addMouseMotionListener(ih);
-        
-        game.init();
-        window.add(game);
-        
-        try {
-            window.setIconImage(ImageIO.read(Main.class.getResourceAsStream("/icon.png")));
-        } catch (IOException e) {
-            logger.error("Could not load icon image", e);
-        }
-
-        window.setLocationRelativeTo(null);
-        window.setResizable(false);
-        window.pack();
-        window.setVisible(true);
-
-        game.start();
-    }
-
-    @Override
-    public void windowClosing(WindowEvent arg0) {
-        game.stop();
-        System.exit(0);
+        game.createWindow();
     }
 
     @Override
     public void init() {
-        initLog4J();
         logger.setLevel(Level.ALL);
 
         game = new Game();
@@ -94,40 +45,5 @@ public class Main extends Applet implements WindowListener {
     @Override
     public void stop() {
         game.stop();
-    }
-
-    public static void initLog4J() {
-        logger = Logger.getLogger(Main.class);
-
-        Logger.getRootLogger().addAppender(new ConsoleAppender(new PatternLayout("%-4r [%t] %-5p %c %x - %m%n")));
-        try {
-            Logger.getRootLogger().addAppender(new FileAppender(new PatternLayout("%-4r [%t] %-5p %c %x - %m%n"), logfile));
-        } catch (IOException e) {
-            logger.error("Could not write log file", e);
-        }
-    }
-
-    @Override
-    public void windowClosed(WindowEvent arg0) {
-    }
-
-    @Override
-    public void windowActivated(WindowEvent arg0) {
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent arg0) {
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent arg0) {
-    }
-
-    @Override
-    public void windowIconified(WindowEvent arg0) {
-    }
-
-    @Override
-    public void windowOpened(WindowEvent arg0) {
     }
 }

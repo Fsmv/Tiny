52da893168511c42d1e2150db4527acfa085ec3b
Andrew Kallmeyer <kallmeyeras@gmail.com>
2014-01-07 00:34:58 -0500

    Game can now create a window and run a new thread

--- Tiny2D/C++/include/Game.h
+++ Tiny2D/C++/include/Game.h
@@ -1,12 +1,19 @@
+#pragma once
+#include "SDL.h"
+#include "SDL_thread.h"
+
 namespace Tiny2D {
 class Game {
     public:
         Game(const char *title = "Game", int width = 720, int height = 480);
         ~Game();
-
+        
+        //static because SDL_CreateThreads needs a C function 
+        static int run(void *data);
         void init();
         void start();
         void stop();
+        void createWindow();
     
     private:
         const char *title;
@@ -15,5 +22,10 @@ class Game {
         int fps = 60;
         int tps = 60;
         bool printFPS = true;
+        bool running = false;
+
+        SDL_Thread *thread = nullptr;
+        SDL_Window *window = nullptr;
+        SDL_Surface *surface = nullptr;
 };
 }
--- Tiny2D/C++/lib/Game.cpp
+++ Tiny2D/C++/lib/Game.cpp
@@ -1,22 +1,58 @@
 #include "Game.h"
+#include "Logger.h"
 
 using namespace Tiny2D;
+
+Logger logger("Game");
 
 Game::Game(const char *title, int width, int height) : title(title), width(width), height(height) {
 }
 
 Game::~Game() {
-    this->stop();
+}
+
+int Game::run(void *data) {
+    Game *g = (Game*) data;
+
+    while(g->running){
+        logger.debug("run");
+    }
 }
 
 void Game::init() {
-    
+    logger.debug("Initializing game");
 }
 
 void Game::start() {
-
+    logger.debug("Starting game");
+    running = true;
+    this->thread = SDL_CreateThread(&Game::run, "Game", this);
 }
 
 void Game::stop() {
+    logger.debug("Stopping game");
+    running = false;
 
+    SDL_WaitThread(this->thread, nullptr);
+
+    SDL_DestroyWindow(this->window);
+    this->window = nullptr;
+    SDL_Quit();
+}
+
+void Game::createWindow() {
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        logger.error("SDL could not initialize");
+        logger.error(SDL_GetError());
+    }else{
+        window = SDL_CreateWindow(this->title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                  this->width, this->height, SDL_WINDOW_SHOWN);
+
+        if(this->window == nullptr) {
+            logger.error("SDL could not create window");
+            logger.error(SDL_GetError());
+        }else{
+            this->surface = SDL_GetWindowSurface(this->window);
+        }
+    }
 }

6cf69bf49ef181b0d775b82bc959186c3f544965
Andrew Kallmeyer <kallmeyeras@gmail.com>
2014-01-20 00:33:14 -0500

    Added setters for game properties

--- Tiny2D/C++/include/Game.h
+++ Tiny2D/C++/include/Game.h
@@ -21,6 +21,48 @@ class Game {
          */
         void stop();
     
+        /**
+         * Set the location of the window icon image
+         *
+         * if null (or not found), do not set an icon
+         * default: "/icon.png"
+         *
+         * @param icon location of the icon image
+         */
+        void setIcon(const char *icon);
+        /**
+         * Sets the frames per second to limit rendering to
+         *
+         * if <= 0 unlimited
+         * default: 60
+         *
+         * @param fps the fps to set
+         */
+        void setFps(int fps);
+        /**
+         * Sets the number of updates per second to limit the game to
+         *
+         * if <= 0 unlimited
+         * 
+         * @param tps the tps to set
+         */
+        void setTps(int tps);
+        /**
+         * Sets the color to use when clearing the screen
+         *
+         * default: 0x00FFFFFF
+         *
+         * @param color the color to set to (in the format 0x00RRGGBB)
+         */
+        void setBackgroundColor(unsigned int color);
+        /**
+         * Sets whether or not the engine should print the current fps and tps
+         *
+         * default: true
+         *
+         * @param printFps if true print the fps and tps on stdout
+         */
+        void setPrintFps(bool printFps);
     private:
         void tick(unsigned int dt);
         void render();
@@ -32,10 +74,15 @@ class Game {
         const char *title;
         int width;
         int height;
+        const char *icon = "/icon.png";
         int fps = 60;
         int tps = 60;
-        bool printFPS = true;
+        bool changedRates = false;
+        bool limitFps = true;
+        bool limitTps = true;
+        bool printFps = true;
         bool running = false;
+        unsigned int background = -1;
 
         SDL_Thread *thread = nullptr;
         SDL_Window *window = nullptr;
--- Tiny2D/C++/lib/Game.cpp
+++ Tiny2D/C++/lib/Game.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "SDL_image.h"
 #include "Game.h"
 #include "utils/Logger.h"
 
@@ -41,12 +42,22 @@ int Game::run(void *data) {
     int tickCount = 0;
 
     while(g->running){
+        if(g->changedRates) {
+            if(g->limitTps)
+                msPerTick = 1000.0/g->tps;
+
+            if(g->limitFps)
+                msPerFrame = 1000.0/g->fps;
+
+            g->changedRates = false;
+        }
+
         unsigned int now = SDL_GetTicks();
         unprocessedTicks += (now - lastUpdateTime) / msPerTick;
         unrenderedFrames += (now - lastUpdateTime) / msPerFrame;
         lastUpdateTime = now;
 
-        if(unprocessedTicks >= 1.0) {
+        if(!g->limitTps || unprocessedTicks >= 1.0) {
             g->tick(now - lastTickTime);
             unprocessedTicks -= 1.0;
             tickCount++;
@@ -54,13 +65,13 @@ int Game::run(void *data) {
             lastTickTime = now;
         }
 
-        if(unrenderedFrames >= 1.0) {
+        if(!g->limitFps || unrenderedFrames >= 1.0) {
             g->render();
             unrenderedFrames -= 1.0;
             frameCount++;
         }
 
-        if(g->printFPS && SDL_GetTicks() - lastFpsTime > 1000) {
+        if(g->printFps && SDL_GetTicks() - lastFpsTime > 1000) {
             //don't use logger for this so we don't clutter the log file
             printf("%d fps; %d tps", frameCount, tickCount);
             frameCount = tickCount = 0;
@@ -100,7 +111,50 @@ void Game::createWindow() {
             logger.error("SDL could not create window");
             logger.error(SDL_GetError());
         }else{
+            this->setIcon(this->icon);
+
             this->surface = SDL_GetWindowSurface(this->window);
+
+            this->setBackgroundColor(this->background);
         }
     }
+}
+
+void Game::setIcon(const char *icon) {
+    this->icon = icon;
+
+    if(this->window != nullptr) {
+        SDL_Surface *image = IMG_Load(icon);
+        
+        if(image != nullptr) {
+            SDL_SetWindowIcon(this->window, image);
+            SDL_FreeSurface(image);
+        }
+    }
+}
+
+void Game::setFps(int fps) {
+    this->fps = fps;
+
+    this->limitFps = fps <= 0 ? false : true;
+    this->changedRates = true;
+}
+
+void Game::setTps(int tps) {
+    this->tps = tps;
+    
+    this->limitTps = tps <= 0 ? false : true;
+    this->changedRates = true;
+}
+
+void Game::setBackgroundColor(unsigned int color) {
+    if(this->surface == nullptr) {
+        this->background = color;
+    }else{
+        this->background = SDL_MapRGB(this->surface->format, (color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF);
+    }
+}
+
+void Game::setPrintFps(bool printFps) {
+    this->printFps = printFps;
 }

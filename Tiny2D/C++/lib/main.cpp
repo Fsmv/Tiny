afa4ee68ad9d8fb944b614501f9a75861fa40b36
Andrew Kallmeyer <kallmeyeras@gmail.com>
2014-01-21 02:52:35 -0500

    Updated the test main to use a screen and sprite

--- Tiny2D/C++/lib/main.cpp
+++ Tiny2D/C++/lib/main.cpp
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include "SDL.h"
+
 #include "Game.h"
+#include "graphics/Screen.h"
+#include "graphics/Sprite.h"
+
+class TestScreen : public Tiny2D::Screen {
+public:
+    TestScreen() : Tiny2D::Screen(1280, 720), sprite("./icon.png") { this->translate(-50, -50); }
+
+    void tick(unsigned int dt);
+    void render();
+
+private:
+    Tiny2D::Sprite sprite;
+};
+
+void TestScreen::tick(unsigned int dt) {
+    this->translate(1, 1);
+}
+
+void TestScreen::render() {
+    this->sprite.draw(this->surface, 50, 50);
+    this->sprite.draw(this->surface, 600, 600);
+}
 
 int main(int argc, char *argv[]) {
     Tiny2D::Game game;
-    
+    TestScreen test;
+
     game.start();
-    SDL_Delay(2000);
+    game.setCurrentScreen(&test);
+    SDL_Delay(10000);
     game.stop();
 }

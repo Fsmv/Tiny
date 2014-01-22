b6180ff0f41b1a5a21a34aab0f00f2e1a55d836a
Andrew Kallmeyer <kallmeyeras@gmail.com>
2014-01-21 19:54:50 -0500

    Updated the test main to use animation

--- Tiny2D/C++/lib/main.cpp
+++ Tiny2D/C++/lib/main.cpp
@@ -4,25 +4,28 @@
 #include "Game.h"
 #include "graphics/Screen.h"
 #include "graphics/Sprite.h"
+#include "graphics/Animation.h"
 
 class TestScreen : public Tiny2D::Screen {
 public:
-    TestScreen() : Tiny2D::Screen(1280, 720), sprite("./icon.png") { this->translate(-50, -50); }
+    TestScreen() : Tiny2D::Screen(1280, 720), sprite("./icon.png"), anim("./anim.png", 4, 32, 32) { this->translate(-50, -50); anim.play(); }
 
     void tick(unsigned int dt);
     void render();
 
 private:
     Tiny2D::Sprite sprite;
+    Tiny2D::Animation anim;
 };
 
 void TestScreen::tick(unsigned int dt) {
     this->translate(1, 1);
+    anim.tick(dt);
 }
 
 void TestScreen::render() {
     this->sprite.draw(this->surface, 50, 50);
-    this->sprite.draw(this->surface, 600, 600);
+    this->anim.draw(this->surface, 600, 600);
 }
 
 int main(int argc, char *argv[]) {
